6da3ffaf68c00e1f8ae86d51e652a6c6cdbaa85a
Caleb Rutan <caleb.rutan@deptagency.com>
2023-04-06 19:17:34 +0000

    convert Auth0 -> Honeycomb for non-logged in button

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -5,6 +5,8 @@ import Auth0Provider from "next-auth/providers/auth0";
 export const authOptions = {
   providers: [
     Auth0Provider({
+      name: "Honeycomb",
+      id: "auth0",
       clientId: process.env.AUTH0_CLIENT_ID,
       clientSecret: process.env.AUTH0_CLIENT_SECRET,
       issuer: process.env.AUTH0_ISSUER_BASE_URL,
